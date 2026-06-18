bf51682ed2873bca231c9d7a0fb5b2ea65f54b61
James McDuffie <409331+omwah@users.noreply.github.com>
2026-06-17 21:50:12 -0700

    Add _ComponentsPickerPanel to allow select which loose components to install into which subsystem of the EngineRoom

--- edge/tui/widgets.py
+++ edge/tui/widgets.py
@@ -390,19 +390,18 @@ class ClickableEntry(Static):
     """
 
     class Picked(Message):
-        def __init__(self, dest: str, ref: int | None = None) -> None:
+        def __init__(self, dest: str, ref: int | str | None = None) -> None:
             self.dest = dest
             self.ref = ref  # an optional target id (e.g. a discovery to salvage)
             super().__init__()
 
-    def __init__(self, markup: str, dest: str, ref: int | None = None, **kwargs: object) -> None:
+    def __init__(self, markup: str, dest: str, ref: int | str | None = None, **kwargs: object) -> None:
         super().__init__(markup, **kwargs)
         self._dest = dest
         self._ref = ref
 
     def on_click(self) -> None:
         self.post_message(self.Picked(self._dest, self._ref))
-
 
 class WarpButton(Button):
     """A single clickable warp affordance."""
